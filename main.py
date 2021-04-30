0a0f6ef3322a1880aa0849420bd42f4be5fb0989
Hampie91 <harmjanwassink91@gmail.com>
2021-04-30 16:11:06 +0200

    Able to load a image in and get a thumbnail

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from tkinter import Tk, filedialog, Button
-import PIL
+from tkinter import Tk, filedialog, Button, Canvas, PhotoImage, Label
+from PIL import ImageTk, Image
 
 def open_file():
-    file = filedialog.askopenfilename()
+    canvas = Canvas(window, width=400, height=400)
+
+    canvas.grid(row=2, column=2, columnspan=3)
+    pic = filedialog.askopenfilename()
+    with Image.open(pic) as im:
+        (width, height) = (im.width // 7, im.height // 7)
+        img = im.resize((width, height))
+        img = ImageTk.PhotoImage(img)
+        canvas.create_image(200, 200, image=img)
+        canvas.image = img  # keep reference to the image
 
 
 
